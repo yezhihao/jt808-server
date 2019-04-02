0b6c68a6edadf39b831391f7f13083d6e27e85d2
Yezhihao <ye.zh@nopluz.com>
2019-04-02 19:10:47 +0800

    实现808协议中编码的转义部分

--- src/main/java/org/yzh/web/jt808/codec/JT808MessageEncoder.java
+++ src/main/java/org/yzh/web/jt808/codec/JT808MessageEncoder.java
@@ -8,7 +8,14 @@ import org.yzh.framework.message.PackageData;
 import org.yzh.web.jt808.dto.basics.Header;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * 808协议解码器
+ *
+ * @author zhihao.ye (yezhihaoo@gmail.com)
+ */
 public class JT808MessageEncoder extends MessageEncoder<Header> {
 
     public JT808MessageEncoder(Charset charset) {
@@ -28,8 +35,61 @@ public class JT808MessageEncoder extends MessageEncoder<Header> {
         byte checkCode = BitOperator.xor(allBuf);
 
         allBuf.writeByte(checkCode);
+        allBuf = escape(allBuf);
 
         return allBuf;
     }
 
+    /**
+     * 转义处理
+     */
+    private static ByteBuf escape(ByteBuf source) {
+        int low = source.readerIndex();
+        int high = source.writerIndex();
+
+        int mark = source.forEachByte(low, high, value -> !(value == 0x7d || value == 0x7e));
+
+        if (mark == -1)
+            return source;
+
+        List<ByteBuf> bufList = new ArrayList<>(5);
+
+        int len;
+        do {
+
+            len = mark + 1 - low;
+            ByteBuf[] slice = slice(source, low, len);
+            bufList.add(slice[0]);
+            bufList.add(slice[1]);
+            low += len;
+
+            mark = source.forEachByte(low, high - low, value -> !(value == 0x7d || value == 0x7e));
+        } while (mark > 0);
+
+        bufList.add(source.slice(low, high - low));
+
+        ByteBuf[] bufs = bufList.toArray(new ByteBuf[bufList.size()]);
+
+        return Unpooled.wrappedBuffer(bufs);
+    }
+
+    /**
+     * 截断转义前报文，并转义
+     */
+    private static ByteBuf[] slice(ByteBuf byteBuf, int index, int length) {
+        byte first = byteBuf.getByte(index + length - 1);
+
+        ByteBuf[] bufs = new ByteBuf[2];
+        bufs[0] = byteBuf.slice(index, length);
+
+        if (first == 0x7d)
+            // 0x01 不做处理 p47
+            bufs[1] = Unpooled.wrappedBuffer(new byte[]{0x01});
+        else {
+            byteBuf.setByte(index + length - 1, 0x7d);
+            bufs[1] = Unpooled.wrappedBuffer(new byte[]{0x02});
+        }
+
+        return bufs;
+    }
 }
