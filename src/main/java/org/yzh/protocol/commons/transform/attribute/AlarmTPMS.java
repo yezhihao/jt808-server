6d58031b6ec59d30646eab2248bf8a2cd683bc1e
剑器近 <zhihao.ye@qq.com>
2020-10-20 19:02:36 +0800

    update

--- src/main/java/org/yzh/protocol/commons/transform/attribute/AlarmTPMS.java
+++ src/main/java/org/yzh/protocol/commons/transform/attribute/AlarmTPMS.java
@@ -6,6 +6,7 @@ import org.yzh.protocol.commons.transform.Attribute;
 import org.yzh.protocol.jsatl12.AlarmId;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 轮胎气压监测系统
@@ -23,6 +24,8 @@ public class AlarmTPMS extends Attribute {
     private LocalDateTime dateTime;
     private int status;
     private AlarmId alarmId;
+    private int total;
+    private List<Item> items;
 
     @Override
     public int getAttributeId() {
@@ -108,5 +111,88 @@ public class AlarmTPMS extends Attribute {
 
     public void setAlarmId(AlarmId alarmId) {
         this.alarmId = alarmId;
+    }
+
+    @Field(index = 39, type = DataType.BYTE, desc = "数据项个数")
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    @Field(index = 40, type = DataType.LIST, desc = "区域列表")
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+        this.total = items.size();
+    }
+
+    public static class Item {
+        private int position;
+        private int type;
+        private int pressure;
+        private int temperature;
+        private int batteryLevel;
+
+        public Item() {
+        }
+
+        public Item(int position, int type, int pressure, int temperature, int batteryLevel) {
+            this.position = position;
+            this.type = type;
+            this.pressure = pressure;
+            this.temperature = temperature;
+            this.batteryLevel = batteryLevel;
+        }
+
+        @Field(index = 0, type = DataType.BYTE, desc = "胎压报警位置")
+        public int getPosition() {
+            return position;
+        }
+
+        public void setPosition(int position) {
+            this.position = position;
+        }
+
+        @Field(index = 2, type = DataType.DWORD, desc = "报警类型")
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        @Field(index = 4, type = DataType.DWORD, desc = "胎压")
+        public int getPressure() {
+            return pressure;
+        }
+
+        public void setPressure(int pressure) {
+            this.pressure = pressure;
+        }
+
+        @Field(index = 6, type = DataType.DWORD, desc = "温度")
+        public int getTemperature() {
+            return temperature;
+        }
+
+        public void setTemperature(int temperature) {
+            this.temperature = temperature;
+        }
+
+        @Field(index = 8, type = DataType.DWORD, desc = "电池电量")
+        public int getBatteryLevel() {
+            return batteryLevel;
+        }
+
+        public void setBatteryLevel(int batteryLevel) {
+            this.batteryLevel = batteryLevel;
+        }
     }
 }
