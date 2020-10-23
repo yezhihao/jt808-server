e1220c50e6d3a84797cd3ca3b7a7672c0a76ddf4
剑器近 <zhihao.ye@qq.com>
2020-10-23 20:21:29 +0800

    update

--- src/main/java/org/yzh/protocol/commons/transform/AttributeConverter.java
+++ src/main/java/org/yzh/protocol/commons/transform/AttributeConverter.java
@@ -1,17 +1,38 @@
 package org.yzh.protocol.commons.transform;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yzh.framework.orm.IdStrategy;
+import org.yzh.framework.orm.Schema;
 import org.yzh.framework.orm.converter.Converter;
 
 public class AttributeConverter implements Converter {
 
+    private static final Logger log = LoggerFactory.getLogger(AttributeConverter.class);
+
+    private static final IdStrategy INSTANCE = AttributeTypes.INSTANCE;
+
     @Override
     public Object convert(Integer key, ByteBuf input) {
-        return AttributeTypes.INSTANCE.readFrom(key, input);
+        Schema schema = INSTANCE.getSchema(key);
+        if (schema != null)
+            return INSTANCE.readFrom(key, input);
+        byte[] bytes = new byte[input.readableBytes()];
+        input.readBytes(bytes);
+        log.warn("未识别的附加信息：ID[{}], HEX[{}]", key, ByteBufUtil.hexDump(bytes));
+        return bytes;
+
     }
 
     @Override
     public void convert(Integer key, ByteBuf output, Object value) {
-        AttributeTypes.INSTANCE.writeTo(key, output, value);
+        Schema schema = INSTANCE.getSchema(key);
+        if (schema != null) {
+            schema.writeTo(output, value);
+        } else {
+            log.warn("未注册的附加信息：ID[{}], Value[{}]", key, value);
+        }
     }
 }
