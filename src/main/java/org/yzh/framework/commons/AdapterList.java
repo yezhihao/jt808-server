2efb9958795506e44a8288045d0cc78a88796663
剑器近 <zhihao.ye@qq.com>
2020-10-27 16:51:59 +0800

    update

--- src/main/java/org/yzh/framework/commons/AdapterList.java
+++ src/main/java/org/yzh/framework/commons/AdapterList.java
@@ -2,10 +2,9 @@ package org.yzh.framework.commons;
 
 import java.util.AbstractList;
 import java.util.List;
-import java.util.RandomAccess;
 import java.util.function.Function;
 
-public final class AdapterList<S, T> extends AbstractList<T> implements RandomAccess {
+public final class AdapterList<S, T> extends AbstractList<T> {
 
     private final List<S> src;
     private final Function<S, T> function;
