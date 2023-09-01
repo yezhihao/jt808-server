6839bbe510069460342f95cb74c1aca8dc184b77
剑器近 <zhihao.ye@qq.com>
2023-09-01 17:29:37 +0800

    更新依赖库

--- 协议文档/jt-start.sh
+++ 协议文档/jt-start.sh
@@ -6,21 +6,27 @@ JAR=jt808-server-1.0.0-SNAPSHOT.jar
 #启动指定后缀的application.yml
 ENV=test
 
-pid=`ps -ef | grep $JAR | grep -v grep | awk '{print $2}'`
+CMD='java -jar '$JAR' --spring.profiles.active='$ENV
+echo $CMD
+
+pid=`ps -ef | grep "$CMD" | grep -v grep | awk '{print $2}'`
 if [ -n "$pid" ];then
     kill -15 $pid
     echo kill -15 $pid
     sleep 1
 fi
 
-pid=`ps -ef | grep $JAR | grep -v grep | awk '{print $2}'`
+pid=`ps -ef | grep "$CMD" | grep -v grep | awk '{print $2}'`
 if [ -n "$pid" ];then
     sleep 2
     kill -9 $pid
     echo kill -9 $pid
 fi
 
-echo starting ...
-nohup java -jar $JAR --spring.profiles.active=$ENV > /dev/null 2>&1 &
-echo start success!
-echo -e "\n"
+if [ "$1" != "stop" ];then
+    echo starting ...
+    nohup $CMD > /dev/null 2>&1 &
+    echo $!
+    echo $! > /var/run/$JAR.$ENV.pid
+    echo success
+fi
